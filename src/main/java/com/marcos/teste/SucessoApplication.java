5e03dc8b64e6adef18ca9933a2dfd6a5d5546f1d
Marcos Vicente <vicentevieira300@gmail.com>
2020-08-17 01:57:50 -0300

    primeiro commit

---
+++ src/main/java/com/marcos/teste/SucessoApplication.java
@@ -0,0 +1,13 @@
+package com.marcos.teste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SucessoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SucessoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcos/teste/SucessoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcos.teste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SucessoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
